5699cd311089991f758d1e6a7d722f71261ed357
dreadster3 <afonso.antunes@live.com.pt>
2024-12-13 00:49:59 +0000

    day 5 part 2 solution

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,12 +1,6 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 advent_of_code::solution!(5);
-
-enum Compare {
-    Higher,
-    Equal,
-    Lower,
-}
 
 fn compare(order: HashMap<u32, Vec<u32>>, a: u32, b: u32) -> Option<bool> {
     if let Some(values) = order.get(&a) {
@@ -42,8 +36,6 @@ pub fn part_one(input: &str) -> Option<u32> {
             acc
         });
 
-    println!("{:?}", order);
-
     let result: u32 = input
         .lines()
         .skip_while(|l| !l.is_empty())
@@ -60,7 +52,46 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let order = input
+        .lines()
+        .take_while(|l| !l.is_empty())
+        .map(|s| (s.split("|").next().unwrap(), s.split("|").last().unwrap()))
+        .map(|(k, v)| {
+            (
+                u32::from_str_radix(k, 10).unwrap(),
+                u32::from_str_radix(v, 10).unwrap(),
+            )
+        })
+        .fold(HashMap::<u32, Vec<u32>>::new(), |mut acc, (k, v)| {
+            acc.entry(k)
+                .and_modify(|vec| vec.push(v))
+                .or_insert(vec![v]);
+            acc
+        });
+
+    let result: u32 = input
+        .lines()
+        .skip_while(|l| !l.is_empty())
+        .skip(1)
+        .map(|l| l.split(",").map(|n| u32::from_str_radix(n, 10).unwrap()))
+        .filter(|v| {
+            !v.clone()
+                .is_sorted_by(|&a, &b| compare(order.clone(), a, b).unwrap())
+        })
+        .map(|v| {
+            let mut vec = v.collect::<Vec<u32>>();
+            vec.sort_by(|&a, &b| {
+                if compare(order.clone(), a, b).unwrap() {
+                    return Ordering::Greater;
+                }
+
+                return Ordering::Less;
+            });
+            vec
+        })
+        .map(|v| *v.iter().nth(v.iter().count() / 2).unwrap())
+        .sum();
+    Some(result)
 }
 
 #[cfg(test)]
@@ -76,6 +107,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
